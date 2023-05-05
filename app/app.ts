eb4102e93c9e6f6502c0ffeef8882a8816847189
Jipson27 <132227636+Jipson27@users.noreply.github.com>
2023-05-04 20:31:29 -0500

    Proyectob1

---
+++ app/app.ts
@@ -0,0 +1,17 @@
+console.log('Hola mundo')
+
+//nula
+let variablenula:null = null
+// indefinido
+// booleano
+let e: boolean = true
+let f: boolean = false 
+// numerico
+let myNumber
+// String o caracteres
+let a: string = 0 + "acaddad"; //ok
+// console.log();
+// simbolo
+// objeto
+
+
---
+++ js/app.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log('Hola mundo');
+//nula
+let variablenula = null;
+// indefinido
+// booleano
+let e = true;
+let f = false;
+// numerico
+let myNumber;
+// String o caracteres
+let a = 0 + "acaddad"; //ok
+// console.log();
+// simbolo
+// objeto
